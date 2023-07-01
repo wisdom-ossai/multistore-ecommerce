768f83defc7014358236a2426d6f779c1b808ff7
Wisdom Ossai <ossaiwisdom48@gmail.com>
2023-07-01 01:25:08 +0100

    implement authentication

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import React, { ReactNode } from "react";
+
+const AuthLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="h-full w-full flex items-center justify-center">
+      {children}
+    </div>
+  );
+};
+
+export default AuthLayout;
